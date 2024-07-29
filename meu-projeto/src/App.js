d1342309a3cae6f36538eeb4a8aa7e160cecad3b
Ermeson Ramos de Lima <ermesonramos1234@gmail.com>
2024-07-29 20:52:23 -0300

    trabalhando com props - 07

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,26 +1,20 @@
 import './App.css';
 import HelloWord from './components/HelloWord';
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
 
 
 function App() {
-  const name = 'Ermeson';
 
-  const newName = name.toUpperCase();
-
-  function sum(a, b) {
-    return a + b;
-  }
-
-    const url = 'https://via.placeholder.com/150'
+    const nome = 'Eduardo';
 
   return (
     <div className="App">
-      <h2>Alterando JSX</h2>
-      <p>Olá meu nome é {newName}</p>
-      <p>Soma: {sum(2, 2)}</p>
-      <img src={url} alt="Minha imagem"/>
-      <HelloWord/>
-
+      <SayMyName nome="Ermeson"/>
+      <SayMyName nome="Matheus"/>
+      <SayMyName nome={nome}/>
+      <Pessoa nome="Ermeson" idade="17" profissao="Programador" foto="https//via.placeholder.com/150"/>
+      
     </div>
   );
 }
