4faa367a25bbea0adc1c6254e99a74620007cb04
Ermeson Ramos de Lima <ermesonramos1234@gmail.com>
2024-07-29 16:45:08 -0300

    Entendendo JSX - 03

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
 
 function App() {
+  const name = 'Ermeson';
+
+  const newName = name.toUpperCase();
+
+  function sum(a, b) {
+    return a + b;
+  }
+
+    const url = 'https://via.placeholder.com/150'
+
   return (
     <div className="App">
-      <h1>Olá, Mundo</h1>
-      <p>Meu primeiro App</p>
+      <h2>Alterando JSX</h2>
+      <p>Olá meu nome é {newName}</p>
+      <p>Soma: {sum(2, 2)}</p>
+      <img src={url} alt="Minha imagem"/>
     </div>
   );
 }
