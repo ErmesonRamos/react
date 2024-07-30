e89103a58a4b1ec09640879bcf4eabc58ccda53e
Ermeson Ramos de Lima <ermesonramos1234@gmail.com>
2024-07-29 23:21:07 -0300

    Inserindo CSS no react - 06

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import HelloWord from './components/HelloWord';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
+import Frase from './components/Frase';
 
 
 function App() {
@@ -10,11 +11,12 @@ function App() {
 
   return (
     <div className="App">
+      <h1>Testando o Estilo</h1>
       <SayMyName nome="Ermeson"/>
       <SayMyName nome="Matheus"/>
       <SayMyName nome={nome}/>
       <Pessoa nome="Ermeson" idade="17" profissao="Programador" foto="https//via.placeholder.com/150"/>
-      
+      <Frase/>
     </div>
   );
 }
