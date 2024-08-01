a98d89c1caab16625d77298ecf3eec47207c070b
Ermeson Ramos de Lima <ermesonramos1234@gmail.com>
2024-07-31 23:40:29 -0300

    Utilizando React fragments - 08

---
+++ meu_projeto/src/App.js
@@ -0,0 +1,13 @@
+import Titulo from './components/Titulo';
+import List from './components/List';
+function App() {
+  return (
+    <div>
+      <Titulo nome="Ermeson" idade="17" profissao="Desenvolver Full-Stack"/>
+      <h1>Olá, Mundo</h1>
+      <List />
+    </div>
+  );
+}
+
+export default App;
---
+++ meu_projeto/src/components/Titulo.js
@@ -0,0 +1,10 @@
+function Titulo({nome, idade, profissao}) {
+    return(
+        <div>
+            <p>Olá meu nome é: {nome} </p>
+            <p>tenho a idade de {idade} anos </p>
+            <p>e quero trabalhar com {profissao} </p>
+        </div>
+    );
+};
+export default Titulo;
