fb1d3d61e24ececffc6d074949b2085bb4c34ec6
Ermeson Ramos de Lima <ermesonramos1234@gmail.com>
2024-08-12 23:57:04 -0300

    erro no dev mas foi corrigido

--- meu_projeto/src/components/List.js
+++ meu_projeto/src/components/List.js
@@ -1,11 +1,13 @@
 import Item from "./Item";
+
 function List() {
     return(
         <>
             <h1>Minha Lista</h1>
             <ul>
-                <Item marca="Ferrari"/>
-                <Item marca="Fiat"/>
+                <Item marca="Ferrari" ano_lancamento={2015} />
+                <Item marca="Fiat" ano_lancamento={1990} />
+                <Item marca="Celta" ano_lancamento={2005} />
             </ul>
         </>
     );
